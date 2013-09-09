14014f6971e9bbb8522a0e43807df80bf66ee3c3
Erik Westrup <erik.westrup@gmail.com>
2013-09-09 15:43:10 +0200

    Don't calc exec time.

--- marking/src/sampler.py
+++ marking/src/sampler.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 # Generate test data from samples of marking usiing different combinations of R[123] and H.
 
-# TODO measure wall clock time
 # TODO calc mean, stddev etc
 # TODO collect all data in a matrix or such
 
 import sys
-import time
 
 import numpy
 
@@ -28,7 +26,6 @@ def main():
         for rand_proc in (R1(nbr_nodes), R2(nbr_nodes), R3(nbr_nodes)):
             #print_log("Using random process {:s}".format(rand_proc.name), 4)
             nbr_rounds = []
-            exec_times = []
             for sample in range(0, samples):
                 #print_log("Sample number {:d}".format(sample), 8)
                 if proc_num == 3:
@@ -37,29 +34,25 @@ def main():
                     if proc_num == 2:
                         rand_proc = R2(nbr_nodes)
                     marker = Marker(tree_height)
-                start_time = time.time()
                 marker.run(rand_proc)
-                end_time = time.time()
                 #print(marker.status())
                 nbr_rounds.append(marker.mark_count)
-                exec_times.append(end_time - start_time)
             #print(numpy.mean(nbr_rounds))
             stats[tree_height][proc_num] = {}
-            stats[tree_height][proc_num]['mean_time'] = numpy.mean(exec_times)
             stats[tree_height][proc_num]['mean_rounds'] = numpy.mean(nbr_rounds)
             #stats[tree_height][proc_num - 1]['std_dev_rounds'] = 
             proc_num += 1
 
 
     # TODO print latex tabel instread to file
-    print("H | N      | R1              | R2               | R3")
-    print('  |' + (' ' * 10) + ("mRnds | mTime   |  " * 3))
-    print('-' * 66)
-    for height in range(2, max_tree_height):
-        print("{:d} | {:4d}| ".format(height, (2**height - 1)), end='')
-        for proc_num in (1,2,3):
-            print("{:.1f}   {:.8f}     | ".format(stats[height][proc_num]['mean_rounds'], stats[height][proc_num]['mean_time']), end='')
-        print()
+    #print("H | N      | R1              | R2               | R3")
+    #print('  |' + (' ' * 10) + ("mRnds | mTime   |  " * 3))
+    #print('-' * 66)
+    #for height in range(2, max_tree_height):
+        #print("{:d} | {:4d}| ".format(height, (2**height - 1)), end='')
+        #for proc_num in (1,2,3):
+            #print("{:.1f}   {:.8f}     | ".format(stats[height][proc_num]['mean_rounds'], stats[height][proc_num]['mean_time']), end='')
+        #print()
 
     #print(stats)
 
