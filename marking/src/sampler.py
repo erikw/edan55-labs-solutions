24d83de70651958cb1dbd4bd34b27fd1a5767482
Erik Westrup <erik.westrup@gmail.com>
2013-09-09 11:32:25 +0200

    Started sampler.py.

---
+++ marking/src/sampler.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Generate test data from samples of marking usiing different combinations of R[123] and H.
+
+import sys
+
+
+
+
+
+
+
+def main():
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
