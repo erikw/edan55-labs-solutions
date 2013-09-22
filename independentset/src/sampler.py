3378465f035d268cd575452ba90488bae7883a3d
Dmitry Basavin <int13dba@gamma-4.student.lth.se>
2013-09-22 17:12:29 +0200

    Fixed latex printing.

--- independentset/src/sampler.py
+++ independentset/src/sampler.py
@@ -11,37 +11,94 @@ from graph import Graph
 from independent_set import read_datafile
 
 data_path = '../data'
+files = {
+R0: ["g{:d}.in".format(x) for x in [4,30,40,50,60]],
+R1: ["g{:d}.in".format(x) for x in [4,30,40,50,60,70,80,90,100]],
+R2: ["g{:d}.in".format(x) for x in [4,30,40,50,60,70,80,90,100,110,120]]
+}
+
 #files = {
-#R0: ["g{:d}.in".format(x) for x in [4,30,40,50,60]],
-#R1: ["g{:d}.in".format(x) for x in [4,30,40,50,60,70,80,90,100]],
-#R2: ["g{:d}.in".format(x) for x in [4,30,40,50,60,70,80,90,100,110,120]]
+#R0: ["g{:d}.in".format(x) for x in [4,30,]],
+#R1: ["g{:d}.in".format(x) for x in [4,30,40,50]],
+#R2: ["g{:d}.in".format(x) for x in [4,30,40,50,60,70]]
 #}
 
-files = {
-R0: ["g{:d}.in".format(x) for x in [4,30,]],
-R1: ["g{:d}.in".format(x) for x in [4,30,40,50]],
-R2: ["g{:d}.in".format(x) for x in [4,30,40,50,60,70]]
-}
+latex_0 = r"""
+\begin{tikzpicture}
+\begin{axis}[
+  height= 5cm,
+  xlabel=$n$,
+  ylabel={log recursive calls},
+  xmin = 20,  xmax = 150,
+  xtick =       {20,40,60,80,100,120},
+  xticklabels = { $20$, $40$, $60$, $80$, $100$, $120$},
+  x tick label as interval = false,
+  scaled ticks = false
+]
+    \addplot[color=black, mark=x] 
+	coordinates {
+"""
+
+latex_1 = r"""
+};
+    \addlegendentry{R0}
+
+    \addplot[color=black, mark=o] 
+	coordinates {
+"""
+
+latex_2 = r"""
+};
+    \addlegendentry{R1}
+
+    \addplot[color=black, mark=*] 
+	coordinates {
+"""
+
+latex_3 = r"""
+};
+    \addlegendentry{R2}
+\end{axis}
+\end{tikzpicture}
+"""
 
 
 def run(algorithm, files):
-    results = []
+    results = {'nbr_calls' : list()}
     for data_file in files:
         graph = read_datafile("{:s}/{:s}".format(data_path, data_file))
         algorithm.nbr_calls = 0
         algorithm(graph)
-        results.append((len(graph.mates),algorithm.nbr_calls))
+        results['nbr_calls'].append((len(graph.mates), algorithm.nbr_calls))
     return results
 
-def main():
-    for algorithm in [R0, R1, R2]:
-        results = run(algorithm, files[algorithm])
-        print("{:s}:".format(algorithm.__name__))        
-        for result in results:
-            log_calls = math.log(result[1], 2)
-            over_n_calls = math.log(result[1], 2) / result[0]
-            print("\tn={:d}, calls={:d}, log2(calls)={:.3f}, log2(calls)/n={:.3f}".format(result[0], result[1], log_calls, over_n_calls))
+def print_latex_file(results):
+    res_file = open("../docs/myreport/output.data", 'w')
+    res_file.write(latex_0)
+    for size, log_calls in results[0]['log_calls']:
+        res_file.write("\t({:d}, {:f})\n".format(size, log_calls)) 
+    res_file.write(latex_1)
+    for size, log_calls in results[1]['log_calls']:
+        res_file.write("\t({:d}, {:f})\n".format(size, log_calls)) 
+    res_file.write(latex_2)
+    for size, log_calls in results[2]['log_calls']:
+        res_file.write("\t({:d}, {:f})\n".format(size, log_calls)) 
+    res_file.write(latex_3)
 
+
+    res_file.close()
+
+def main():
+    results = []
+    for algorithm in [R0, R1, R2]:
+        results.append(run(algorithm, files[algorithm]))
+        results[-1]['log_calls'] = list()
+        #results[-1]['over_n_calls'] = list()
+        for size, nbr_calls in results[-1]['nbr_calls']:
+            results[-1]['log_calls'].append((size, math.log(nbr_calls, 2)))
+            #results[-1]['over_n_calls'].append(math.log(result[1], 2) / result[0])
+    #print_latex_file(results)
+    print(results)
 
 if __name__ == '__main__':
     sys.exit(main())
